3f9e9e52955a45844b8e5d99c9071e1aeb181aed
NALAMKALYANACHAKRAVARTHI <100521699+KALYAN7637@users.noreply.github.com>
2025-08-04 20:21:19 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,4 +1,4 @@
 DATAOPS_CLIENT_ID = "dataopssuite-restapi-client"
 DATAOPS_CLIENT_SECRET = "O#BJXMDm"
-DATAOPS_USERNAME = "kalyanachakravarthi.nalam"
-DATAOPS_PASSWORD = "U2FsdGVkX19YkV7mUR2pwpqZamUFWqi9CswGrFqywYw="
+DATAOPS_USERNAME = "vijrumbhan"
+DATAOPS_PASSWORD = "U2FsdGVkX190zofymDJqYKiiowPaEt9ganRAK8IgS4k="
